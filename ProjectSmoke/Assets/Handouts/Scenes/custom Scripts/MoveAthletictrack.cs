00410402c6b2c27051e4617fc14d4c2d37718760
Stijn <stijngeessink@hotmail.com>
2025-01-10 11:13:55 +0100

    Atletiekbaan improvements

--- ProjectSmoke/Assets/Handouts/Scenes/custom Scripts/MoveAthletictrack.cs
+++ ProjectSmoke/Assets/Handouts/Scenes/custom Scripts/MoveAthletictrack.cs
@@ -6,6 +6,8 @@ public class MoveAthletictrack : MonoBehaviour
 {
 
     public GameObject Movingfloor;
+    public GameObject Movingperson;
+    public int Movespeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,7 @@ public class MoveAthletictrack : MonoBehaviour
     {
         // Move the object forward along its z axis 1 unit/second.
         Movingfloor.transform.Translate(Vector3.forward * Time.deltaTime);
+        Movingperson.transform.Translate(Vector3.back * Time.deltaTime/Movespeed);
 
     }
 }
